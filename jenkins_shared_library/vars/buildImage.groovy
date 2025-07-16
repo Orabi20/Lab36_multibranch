b8a66a6dd6162f43d7e21e0c23591fd1ae9b4c98
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-16 16:26:02 +0300

    Delete jenkins_shared_library directory

--- jenkins_shared_library/vars/buildImage.groovy
+++
@@ -1,8 +0,0 @@
-def call(String imageName) {
-    dir('Jenkins/Lab35_sharedlib_agent') {
-        sh """
-            docker build -t ${imageName} .
-        """
-    }
-}
-
--- jenkins_shared_library/vars/pushImage.groovy
+++
@@ -1,9 +0,0 @@
-def call(String imageName, String dockerUser, String dockerPass) {
-
-    sh """
-        echo "${dockerPass}" | docker login -u "${dockerUser}" --password-stdin
-        docker push ${imageName}
-        docker logout
-    """
-}
-
