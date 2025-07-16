a76aa10ffdee15e9e0c75ae14b04e86cef19c4a9
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-16 13:04:11 +0300

    Update deployOnK8s.groovy

--- jenkins_shared_library/vars/deployOnK8s.groovy
+++ jenkins_shared_library/vars/deployOnK8s.groovy
@@ -4,15 +4,18 @@ def call(String apiServer, String token, String namespace, String imageName, Str
 
     dir(dirPath) {
         sh """
+            echo "Replacing image with: ${imageName}"
             sed -i 's|image:.*|image: ${imageName}|' ${fileName}
+            echo "--- Deployment File After Image Replace ---"
             cat ${fileName}
         """
+
         sh """
-            kubectl --server=${apiServer} \
-                    --token=${token} \
-                    --insecure-skip-tls-verify \
+            echo "Deploying to namespace: ${namespace}"
+            kubectl --server=${apiServer} \\
+                    --token=${token} \\
+                    --insecure-skip-tls-verify \\
                     apply -f ${fileName} -n ${namespace} --validate=false
         """
     }
 }
-
